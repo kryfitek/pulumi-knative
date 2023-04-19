3f19c14e08508c6827936fc58b6431b18a5492d3
kryfitek <kryfitek@gmail.com>
2023-04-18 22:17:12 -0400

    Add cicd namespace

--- infra/cicd/modules/cicd.py
+++ infra/cicd/modules/cicd.py
@@ -1,18 +1,38 @@
 from pulumi import ResourceOptions
 from pulumi_kubernetes.yaml import ConfigGroup
+from pulumi_kubernetes.core.v1 import Namespace
+from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
 
 class Cicd:
     def deploy(self, k8s_provider):
-        cicd = ConfigGroup(
+        cicd_ns = Namespace(
             "cicd",
+            metadata=ObjectMetaArgs(
+                name="cicd"
+            ),
+            opts = ResourceOptions(
+                provider=k8s_provider
+            )
+        )
+        tasks = ConfigGroup(
+            "tasks",
             files = [
                 "manifests/rbac/*.yaml",
                 "manifests/secrets/*.yaml",
-                "manifests/tasks/*.yaml",
+                "manifests/tasks/*.yaml"
+            ], opts = ResourceOptions(
+                provider=k8s_provider,
+                parent=cicd_ns
+            )
+        )
+        pipes = ConfigGroup(
+            "pipes",
+            files = [
                 "manifests/pipelines/*.yaml",
                 "manifests/triggers/*.yaml"
             ], opts = ResourceOptions(
-                provider=k8s_provider
+                provider=k8s_provider,
+                parent=tasks
             )
         )
         ConfigGroup(
@@ -21,6 +41,6 @@ class Cicd:
                 "manifests/taskruns/*.yaml"
             ], opts = ResourceOptions(
                 provider=k8s_provider,
-                parent=cicd
+                parent=pipes
             )
         )
